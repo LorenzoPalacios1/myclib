1e6a0db848ed21703c63847f63382a92d9b6b19a
LorenzoPalacios1 <lp434717@gmail.com>
2024-03-31 16:19:57 -0500

    checkpoint
    Signed-off-by: LorenzoPalacios1 <lp434717@gmail.com>

--- MyBasics.c
+++ MyBasics.c
@@ -1,322 +1,129 @@
-#ifndef _INC_STDLIB
-#include <stdlib.h>
-#endif
-
-#ifndef _INC_STRING
-#include <string.h>
-#endif
-
-#if (!defined __CLANG_LIMITS_H || !defined _GCC_LIMITS_H_)
-#include <limits.h>
-#endif
-
-#ifndef _INC_MYBASICS
 #include "MyBasics.h"
-#endif
 
-// Determines the number of characters that would be within the string equivalent of INT_MAX.
-#define INT_MAX_CHARS (11)
+#include <limits.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
-/*
- * Writes to the first argument, str, from stream until reading the delimiter
- * character, or upon the string reaching the specified length.
- *
- * \returns Returns the length of the string discounting the null terminator, or 0 if nothing was written.
- *
- * \note If this function returns 0, the string will be left unmodified (that is, nothing will be written).
- *
- * \note A null terminator will replace the delimiter within the string.
- *
- * \note Otherwise, if 'length' characters were written before the delimiter was read, a null terminator will be
- * appended to the end of the string if no delimiter was read or EOF was not met.
- *
- * \note If this occurs, the returned value will be length + 1.
- *
- * \param str The pointer to string that will be written to
- * \param delim The delimiter character
- * \param max_length The maximum size of the string
- * \param stream The input stream to read from
- */
-size_t getStr(char **const str, const char delim, const size_t max_length, FILE *const stream)
-{
-    if (str == NULL)
-    {
-        fputs("\ngetStr(): Invalid pointer-to-pointer-to-char provided; No reading occurred", stderr);
-        return 0;
+size_t getStr(char *str, const char delim, const size_t max_length,
+              FILE *const stream) {
+  if (str == NULL) {
+    fputs("\ngetStr(): Invalid string pointer; No reading occurred", stderr);
+    return 0;
+  }
+  if (stream == NULL) {
+    fputs("\ngetStr(): Invalid input stream; No reading occurred", stderr);
+    return 0;
+  }
+  if (max_length == 0) return 0;
+  if (feof(stream)) return 0;
+
+  /* `max_length + 1` to ensure there is always space for a null terminator */
+  if (str == NULL) {
+    str = malloc(max_length + 1);
+    if (str == NULL) {
+      fputs("\ngetStr(): malloc() failure; No writing occurred", stderr);
+      return 0;
     }
+  }
+  size_t i = 0;
+  for (; i < max_length; ++i) {
+    str[i] = getc(stream);
+    if (str[i] == delim || str[i] == EOF) break;
+  }
+  str[i] = '\0';
 
-    if (max_length == 0)
-    {
-        fprintf(stderr, "\ngetStr(): Invalid string length (%llu); No reading occurred", max_length);
-        return 0;
-    }
-
-    if (stream == NULL)
-    {
-        fputs("\ngetStr(): Invalid input stream provided; No reading occurred", stderr);
-        return 0;
-    }
-
-    if (feof(stream))
-        return 0;
-
-    // "length + 1" to ensure there is always space for a null terminator
-    char buffer[max_length + 1];
-    size_t i = 0;
-    for (; i < max_length; ++i)
-    {
-        buffer[i] = getc(stream);
-        if (buffer[i] == delim || buffer[i] == EOF)
-            break;
-    }
-
-    // If nothing meaningful was read then we can just return 0, leaving the 'str' argument untouched.
-    // This will usually occur if only the specified delimiter character was read from the 'stream'
-    // which gets replaced by the null terminator.
-    if (buffer[0] == '\0')
-        return 0;
-
-    buffer[i] = '\0';
-
-    // Allocating memory for 'str' if the user hadn't already allocated for it themselves
-    if (*str == NULL)
-    {
-        *str = malloc(max_length + 1);
-        if (*str == NULL)
-        {
-            fputs("\ngetStr(): malloc() failure; No reading occurred", stderr);
-            return 0;
-        }
-    }
-
-    strcpy_s(*str, sizeof(buffer), buffer);
-
-    return i;
+  return i;
 }
 
-/*
- * Writes to the first argument, 'str', from stdin until reading a newline character,
- * or upon the string reaching the specified length.
- *
- * The difference between getStr() and this function is that this function has an
- * implicit delimiter, '\\n', an implicit stream, 'stdin', and will automatically flush
- * any unused input from stdin.
- *
- * This function has the same semantics as getStr() aside from the flushing of stdin.
- */
-size_t getStrStdin(char **const str, const size_t length)
-{
-    const size_t numChars = getStr(str, '\n', length, stdin);
-    // If 'numChars' is greater than the passed length, that means getStr() appended a null terminator
-    // due to not encountering a newline, which means there is still input that needs to be flushed,
-    // hence the fseek() call below.
-    if (numChars > length)
-        fseek(stdin, 0, SEEK_END);
-    return numChars;
+size_t getStrStdin(char *str, const size_t max_length) {
+  const size_t str_len = getStr(str, '\n', max_length, stdin);
+  /* Discard any unused input. */
+  if (str_len > max_length)
+    while (getchar() != '\n')
+      ;
+  return str_len;
 }
 
-/*
- * Parses the passed string into an int which will be written to the second argument, 'num'.
- *
- * If this function fails, such as via invalid characters or overflow, 'num' will be left
- * unchanged and ERRCODE_GENERAL will be returned.
- */
-int strToInt(const char *str, int *const num)
-{
-    if (str == NULL)
-    {
-        fputs("\nstrToInt(): 'str' is NULL; No conversion occurred", stderr);
-        return ERRCODE_BAD_PTR;
-    }
+/* Returns a random string of `unsigned char` using `random_vis_uchar()`. */
+unsigned char *randomUnsignedString(const unsigned char min,
+                                    const unsigned char max,
+                                    const size_t length) {
+  if (max < min) {
+    fprintf(stderr,
+            "\ngenerate_test_ustring(): Invalid values for range (%u is NOT "
+            "GREATER THAN %u)",
+            max, min);
+    return NULL;
+  }
+  if (length == 0) {
+    fprintf(stderr, "\ngenerate_test_ustring(): Invalid length: %llu", length);
+    return NULL;
+  }
 
-    if (num == NULL)
-    {
-        fputs("\nstrToInt(): 'num' is NULL; No conversion occurred", stderr);
-        return ERRCODE_BAD_PTR;
-    }
+  /* (length + 1) to account for the null terminator. */
+  unsigned char *str = malloc(length + 1);
+  if (str == NULL) {
+    fputs("\ngenerate_test_ustring(): malloc() failure; returning NULL",
+          stderr);
+    return NULL;
+  }
 
-    const unsigned int MAX_PLACE_VALUE = 1000000000;
+  for (size_t i = 0; i < length; i++)
+    str[i] = randomUnsignedCharInRange(min, max);
 
-    const bool containsSign = (*str == '-' || *str == '+');
-    const bool isNegative = (*str == '-');
+  str[length] = '\0';
 
-    const size_t STARTING_INDEX = containsSign;
-    const size_t ENDING_INDEX = strlen(str) - 1; // "strlen(str) - 1" to discount the null terminator
-
-    /* If there are more characters, valid or not, than an int can hold. */
-    if (ENDING_INDEX - STARTING_INDEX > INT_MAX_CHARS)
-        return ERRCODE_BAD_INPUT;
-
-    unsigned int tempNum = 0; // This will replace 'num' upon the function's successful completion
-    unsigned int placeValue = 1;
-
-    // Iterating over the string in reverse allows for the correct place values to be assigned with
-    // as little hassle as possible.
-    for (unsigned short i = containsSign; i < ENDING_INDEX; i--, placeValue *= 10)
-    {
-        const char currentDigit = charToInt(*str);
-
-        if (currentDigit == -1)
-            return ERRCODE_BAD_INPUT;
-
-        // We can skip over any values of 0.
-        if (currentDigit == 0)
-            continue;
-
-        tempNum += currentDigit * placeValue;
-
-        if (tempNum >= INT_MAX || tempNum / placeValue < 1)
-        {
-            if (tempNum == INT_MAX)
-                break;
-            if (tempNum == (unsigned int)INT_MIN * -1 && isNegative)
-            {
-                *num = INT_MIN;
-                return ERRCODE_SUCCESS;
-            }
-            return ERRCODE_BAD_INPUT;
-        }
-    }
-
-    *num = tempNum;
-
-    if (isNegative)
-        *num *= -1;
-    return ERRCODE_SUCCESS;
+  return str;
 }
 
-// Returns a random string of unsigned chars using random_vis_uchar().
-unsigned char *randomUnsignedString(const unsigned char min, const unsigned char max, const size_t length)
-{
-    if (max < min)
-    {
-        fprintf(stderr, "\ngenerate_test_ustring(): Invalid values for range (%u is NOT GREATER THAN %u)", max, min);
-        return NULL;
-    }
+// Returns a random string of signed chars using `random_vis_uchar()`.
+char *randomString(const char min, const char max, const size_t length) {
+  if (max < min) {
+    fprintf(stderr,
+            "\ngenerate_test_ustring(): Invalid values for range (%u is NOT "
+            "GREATER THAN %u)",
+            max, min);
+    return NULL;
+  }
 
-    if (length == 0)
-    {
-        fprintf(stderr, "\ngenerate_test_ustring(): Invalid length: %llu", length);
-        return NULL;
-    }
+  // (length + 1) to account for the null terminator
+  char *str = malloc(length + 1);
+  if (str == NULL) {
+    fputs("\ngenerate_test_string(): malloc() failure; returning NULL", stderr);
+    return NULL;
+  }
 
-    // (length + 1) to account for the null terminator
-    unsigned char *str = malloc(length + 1);
+  for (size_t i = 0; i < length; i++)
+    str[i] = randomUnsignedCharInRange(min, max);
+  str[length] = '\0';
 
-    if (str == NULL)
-    {
-        fputs("\ngenerate_test_ustring(): malloc() failure; returning NULL", stderr);
-        return NULL;
-    }
-
-    for (size_t i = 0; i < length; i++)
-        str[i] = randomUnsignedCharInRange(min, max);
-
-    str[length] = '\0';
-
-    return str;
+  return str;
 }
 
-// Returns a random string of signed chars using random_vis_uchar().
-char *randomString(const char min, const char max, const size_t length)
-{
-    if (max < min)
-    {
-        fprintf(stderr, "\ngenerate_test_ustring(): Invalid values for range (%u is NOT GREATER THAN %u)", max, min);
-        return NULL;
-    }
+// Returns a random string of alphabetical chararacters using
+// random_vis_uchar().
+char *randomAlphabeticalString(const size_t length) {
+  if (length == 0) {
+    fprintf(stderr, "\ngenerate_test_string_alphabetic(): Invalid length: %llu",
+            length);
+    return NULL;
+  }
 
-    // (length + 1) to account for the null terminator
-    char *str = malloc(length + 1);
-    if (str == NULL)
-    {
-        fputs("\ngenerate_test_string(): malloc() failure; returning NULL", stderr);
-        return NULL;
-    }
+  // (length + 1) to account for the null terminator
+  char *str = malloc(length + 1);
+  if (str == NULL) {
+    fputs("\ngenerate_test_string(): malloc() failure; returning NULL", stderr);
+    return NULL;
+  }
 
-    for (size_t i = 0; i < length; i++)
-        str[i] = randomUnsignedCharInRange(min, max);
+  for (size_t i = 0; i < length; i++) {
+    if (randomBool() & 1)
+      str[i] = randomUnsignedCharInRange('a', 'z');
+    else
+      str[i] = randomUnsignedCharInRange('A', 'Z');
+  }
+  str[length] = '\0';
 
-    str[length] = '\0';
-
-    return str;
-}
-
-// Returns a random string of alphabetical chararacters using random_vis_uchar().
-char *randomAlphabeticalString(const size_t length)
-{
-    if (length == 0)
-    {
-        fprintf(stderr, "\ngenerate_test_string_alphabetic(): Invalid length: %llu", length);
-        return NULL;
-    }
-
-    // (length + 1) to account for the null terminator
-    char *str = malloc(length + 1);
-    if (str == NULL)
-    {
-        fputs("\ngenerate_test_string(): malloc() failure; returning NULL", stderr);
-        return NULL;
-    }
-
-    for (size_t i = 0; i < length; i++)
-    {
-        /* Don't use randomBool() so as to not magically drain caches if they are enabled. */
-        if (rand() & 1)
-            str[i] = randomUnsignedCharInRange('a', 'z');
-        else
-            str[i] = randomUnsignedCharInRange('A', 'Z');
-    }
-    str[length] = '\0';
-
-    return str;
-}
-
-/*
- * Parses up to 11 numerical characters (including a preceding sign) and writes the
- * `int` equivalent (if any) to `num`.
- *
- * Stops upon reading a non-numerical character.
- *
- * \returns `1` if the passed arguments were valid, but no characters in the stream
- * were parsable, or if type int was overflowed.
- */
-int readInt(int *const num, FILE *const stream)
-{
-    if (num == NULL)
-    {
-        fputs("\nreadInt(): Passed int pointer is NULL; No reading occurred", stderr);
-        return ERRCODE_BAD_PTR;
-    }
-
-    if (stream == NULL)
-    {
-        fputs("\nreadInt(): Passed file is NULL; No reading occurred", stderr);
-        return ERRCODE_BAD_FILE;
-    }
-
-    if (feof(stream))
-    {
-        fputs("\nreadInt(): Passed file is already at EOF; No reading occurred", stderr);
-        return ERRCODE_FILE_AT_EOF;
-    }
-
-    /*
-     * `INT_MAX_CHARS + 1` to account for a null terminator.
-     *
-     * Additionally, this buffer can be static since its state is generally irrelevant and
-     * allows for the avoidance of malloc() and free() nonsense when passed as a pointer to
-     * strToInt().
-     */
-    static char buffer[INT_MAX_CHARS + 1];
-
-    unsigned short i = 0;
-    for (; i < INT_MAX_CHARS; i++)
-    {
-        if ((buffer[i] = getc(stream)) == EOF)
-            return ERRCODE_FILE_REACHED_EOF;
-    }
-    buffer[i] = '\0';
-
-    return strToInt(buffer, num);
+  return str;
 }
--- MyBasics.h
+++ MyBasics.h
@@ -1,21 +1,23 @@
-#ifndef _INC_STDIO
-#include <stdio.h>
-#endif
-
-#ifndef _INC_STDBOOL_H
 #include <stdbool.h>
-#endif
+#include <stdio.h>
+#include <stdlib.h>
 
 #ifndef _INC_MYBASICS
 #define _INC_MYBASICS
 
-#define ERRCODE_SUCCESS             (0x0) /* Denotes a successful execution.                                        */
-#define ERRCODE_GENERAL             (0x1) /* An undefined status code; consult the function's documentation.        */
-#define ERRCODE_BAD_PTR             (0x2) /* Denotes a bad pointer argument.                                        */
-#define ERRCODE_BAD_FILE            (0x3) /* Denotes a bad or `NULL` pointer to a `FILE` argument.                  */
-#define ERRCODE_FILE_AT_EOF         (0x4) /* The passed `FILE` is at `EOF` *before* any reading occurred.           */
-#define ERRCODE_BAD_INPUT           (0x5) /* Denotes a bad string (eg. passing alphabetical chars to strToInt())    */
-#define ERRCODE_FILE_REACHED_EOF    (0x6) /* The function reached `EOF` during execution.                           */
+#define ERRCODE_SUCCESS (0x0) /* Denotes a successful execution. */
+#define ERRCODE_GENERAL \
+  (0x1) /* An undefined status code; consult the function's documentation. */
+#define ERRCODE_BAD_PTR (0x2) /* Denotes a bad pointer argument. */
+#define ERRCODE_BAD_FILE \
+  (0x3) /* Denotes a bad or `NULL` pointer to a `FILE` argument. */
+#define ERRCODE_FILE_AT_EOF \
+  (0x4) /* The passed `FILE` is at `EOF` *before* any reading occurred. */
+#define ERRCODE_BAD_INPUT                                                      \
+  (0x5) /* Denotes a bad string (eg. passing alphabetical chars to strToInt()) \
+         */
+#define ERRCODE_FILE_REACHED_EOF \
+  (0x6) /* The function reached `EOF` during execution. */
 
 /* The minimum `signed char` value for a visible character (inclusive). */
 #define VIS_CHAR_START (' ' + 1)
@@ -28,61 +30,86 @@
  */
 #define ALLOW_RANDOM_GEN_CACHING (true)
 
-/* Determines the number of elements (not bytes) that each  cache. */
+// Determines the number of elements (not bytes) that each function will cache.
 #if (ALLOW_RANDOM_GEN_CACHING != 0)
 #define CACHE_SIZE ((size_t)256)
 #endif
 
 /*
- * Determines the maximum number of characters that would be within the string equivalent
- * of `INT_MAX`.
+ * Writes to the first argument, str, from stream until reading the delimiter
+ * character, or upon the string reaching the specified length.
+ *
+ * \returns Returns the length of the string discounting the null terminator, or
+ * 0 if nothing was written.
+ *
+ * \note If this function returns 0, the string will be left unmodified (that
+ * is, nothing will be written).
+ *
+ * \note A null terminator will replace the delimiter within the string.
+ *
+ * \note Otherwise, if 'length' characters were written before the delimiter was
+ * read, a null terminator will be appended to the end of the string if no
+ * delimiter was read or EOF was not met.
+ *
+ * \note If this occurs, the returned value will be length + 1.
+ *
+ * \param str The pointer to string that will be written to
+ * \param delim The delimiter character
+ * \param max_length The maximum size of the string
+ * \param stream The input stream to read from
  */
-#define INT_MAX_CHARS (11)
-
-size_t getStr(char **const str, const char delim, const size_t max_length, FILE *const stream);
-
-size_t getStrStdin(char **const str, const size_t length);
+size_t getStr(char *str, const char delim, const size_t max_length,
+              FILE *const stream);
 
 /*
- * Returns true, if the passed char represents an alphabetical letter.
+ * Writes to the first argument, `str`, from `stdin` until reading a newline
+ * character, or upon the string reaching the specified length.
  *
- * Otherwise returns false.
+ * The difference between `getStr()` and this function is that this function has
+ * an implicit delimiter, `\\n`, an implicit stream, `stdin`, and will
+ * automatically flush any unused input from stdin.
+ *
+ * This function has the same semantics as `getStr()` otherwise.
  */
-static inline int isAlphabetical(const char letter)
-{
-    return ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z'));
+size_t getStrStdin(char *str, const size_t length);
+
+/*
+ * Returns `true` if the passed char represents an alphabetical letter.
+ *
+ * Otherwise returns `false`.
+ */
+inline bool isAlphabetical(const char letter) {
+  return ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z'));
 }
 
 /*
- * Returns non-zero, true, if the passed char represents a number.
+ * Returns `true` if the passed char represents a number.
  *
- * Otherwise, this returns 0, false.
+ * Otherwise returns `false`.
  */
-static inline int isNumerical(const char number)
-{
-    return (number >= '0' && number <= '9');
+inline bool isNumerical(const char number) {
+  return (number >= '0' && number <= '9');
 }
 
 /*
- * Returns true if the passed char represents either a number or an alphabetical letter.
+ * Returns `true` if the passed char represents either a number or an
+ * alphabetical letter.
  *
- * Returns false otherwise.
+ * Otherwise returns `false`.
  */
-static inline bool isAlphaNumerical(const char item)
-{
-    return (isAlphabetical(item) || isNumerical(item));
+inline bool isAlphaNumerical(const char item) {
+  return (isAlphabetical(item) || isNumerical(item));
 }
 
 /*
  * Returns the passed character as an integer singleton (0-9), if possible.
  *
- * If the passed character does not represent an integer, this function returns `-1`.
+ * If the passed character does not represent an integer, this function returns
+ * `-1`.
  */
-static inline int charToInt(const char num)
-{
-    if (isNumerical(num))
-        return num - '0';
-    return -1;
+inline int charToInt(const char num) {
+  if (isNumerical(num)) return num - '0';
+  return -1;
 }
 
 int strToInt(const char *const str, int *const num);
@@ -91,14 +118,15 @@ int strToInt(const char *const str, int *const num);
  * Returns a single `int` from `rand()` within the specified range (inclusive).
  * Always returns `0` if (`max` < `min`).
  */
-static inline int randomInt(const int min, const int max)
-{
-    if (max < min)
-    {
-        fprintf(stderr, "randomInt(): Invalid values for max and min values (%d is NOT GREATER THAN %d)\n", max, min);
-        return 0;
-    }
-    return rand() % (max - min + 1) + min;
+static inline int randomInt(const int min, const int max) {
+  if (max < min) {
+    fprintf(stderr,
+            "randomInt(): Invalid values for max and min values (%d is NOT "
+            "GREATER THAN %d)\n",
+            max, min);
+    return 0;
+  }
+  return rand() % (max - min + 1) + min;
 }
 
 /*
@@ -106,23 +134,26 @@ static inline int randomInt(const int min, const int max)
  *
  * If caching is enabled, the first call to this function will stock the cache.
  */
-static inline unsigned char randomBool(void)
-{
+bool randomBool(void) {
 #if (ALLOW_RANDOM_GEN_CACHING == false)
-    return rand() & 1;
+  return rand() & 1;
 #else
-    static unsigned char cache[CACHE_SIZE];
-    /* Setting the iterator to the end of the cache will prompt its restocking. */
-    static size_t iterator = CACHE_SIZE;
-
-    if (iterator == CACHE_SIZE)
-    {
-        for (size_t i = 0; i < CACHE_SIZE; i++)
-            cache[i] = rand() & 1;
-        iterator = 0;
+  /* For this particular cache, the stored elements are used as bitfields. */
+  static uint_fast8_t cache[CACHE_SIZE];
+  /* Setting the iterator to the end of the cache will prompt its restocking. */
+  static size_t cache_iterator = CACHE_SIZE;
+  static uint_fast8_t bit_iterator = 1;
+  if (bit_iterator == (sizeof(*cache) * CHAR_BIT)) {
+    bit_iterator = 1;
+    cache_iterator++;
+  }
+  if (cache_iterator == CACHE_SIZE) {
+    for (size_t i = 0; i < CACHE_SIZE; ++i) {
+      cache[i] = rand();
     }
-
-    return cache[iterator++];
+    cache_iterator = 0;
+  }
+  return cache[cache_iterator] & (1 << bit_iterator++);
 #endif
 }
 
@@ -130,30 +161,29 @@ char *randomString(const char min, const char max, const size_t length);
 
 char *randomAlphabeticalString(const size_t length);
 
-unsigned char *randomUnsignedString(const unsigned char min, const unsigned char max, const size_t length);
+uint8_t *randomUnsignedString(const uint8_t min, const uint8_t max,
+                              const size_t length);
 
 /*
  * Returns a single visible `unsigned char` from `rand()`.
  *
  * If caching is enabled, the first call to this function will stock the cache.
  */
-static inline unsigned char randomVisibleUnsignedChar(void)
-{
+static inline unsigned char randomVisibleUnsignedChar(void) {
 #if (ALLOW_RANDOM_GEN_CACHING == false)
-    return randomInt(VIS_CHAR_START, UCHAR_MAX);
+  return randomInt(VIS_CHAR_START, UCHAR_MAX);
 #else
-    static unsigned char cache[CACHE_SIZE];
-    /* Setting the iterator to the end of the cache will prompt its restocking. */
-    static size_t iterator = CACHE_SIZE;
+  static unsigned char cache[CACHE_SIZE];
+  static size_t iterator = CACHE_SIZE; /* Setting the iterator to the end of the
+                                          cache will prompt its restocking. */
 
-    if (iterator == CACHE_SIZE)
-    {
-        for (size_t i = 0; i < CACHE_SIZE; i++)
-            cache[i] = randomInt(VIS_CHAR_START, UCHAR_MAX);
-        iterator = 0;
-    }
+  if (iterator == CACHE_SIZE) {
+    for (size_t i = 0; i < CACHE_SIZE; i++)
+      cache[i] = randomInt(VIS_CHAR_START, UCHAR_MAX);
+    iterator = 0;
+  }
 
-    return cache[iterator++];
+  return cache[iterator++];
 #endif
 }
 
@@ -162,37 +192,38 @@ static inline unsigned char randomVisibleUnsignedChar(void)
  *
  * If caching is enabled, the first call to this function will stock the cache.
  */
-static inline unsigned char randomVisibleChar(void)
-{
+static inline unsigned char randomVisibleChar(void) {
 #if (ALLOW_RANDOM_GEN_CACHING == false)
-    return randomInt(VIS_CHAR_START, CHAR_MAX);
+  return randomInt(VIS_CHAR_START, CHAR_MAX);
 #else
-    static unsigned char cache[CACHE_SIZE];
-    static size_t iterator = CACHE_SIZE;
+  static unsigned char cache[CACHE_SIZE];
+  static size_t iterator = CACHE_SIZE; /* Setting the iterator to the end of the
+                                          cache will prompt its restocking. */
 
-    if (iterator == CACHE_SIZE)
-    {
-        for (size_t i = 0; i < CACHE_SIZE; i++)
-            cache[i] = randomInt(VIS_CHAR_START, CHAR_MAX);
-        iterator = 0;
-    }
+  if (iterator == CACHE_SIZE) {
+    for (size_t i = 0; i < CACHE_SIZE; i++)
+      cache[i] = randomInt(VIS_CHAR_START, CHAR_MAX);
+    iterator = 0;
+  }
 
-    return cache[iterator++];
+  return cache[iterator++];
 #endif
 }
 
 /*
- * Returns a single `unsigned char` from `rand()` whose value is between `min` and `max` (inclusive).
+ * Returns a single `unsigned char` from `rand()` whose value is between `min`
+ * and `max` (inclusive).
  */
-static inline unsigned char randomUnsignedCharInRange(const unsigned char min, const unsigned char max)
-{
-    if (max < min)
-    {
-        fprintf(stderr, "random_uchar_range(): Invalid values for max and min values (%u is NOT GREATER THAN %u)\n",
-                max, min);
-        return 0;
-    }
-    return randomInt(min, max);
+static inline unsigned char randomUnsignedCharInRange(const unsigned char min,
+                                                      const unsigned char max) {
+  if (max < min) {
+    fprintf(stderr,
+            "random_uchar_range(): Invalid values for max and min values (%u "
+            "is NOT GREATER THAN %u)\n",
+            max, min);
+    return 0;
+  }
+  return randomInt(min, max);
 }
 
 int readInt(int *const num, FILE *stream);
