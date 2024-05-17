20ad4e8df1ae905b9d405d4bd807f0139159ca89
LorenzoPalacios1 <lp434717@gmail.com>
2024-05-16 20:46:27 -0500

    checkpoint: strextensions
    Signed-off-by: LorenzoPalacios1 <lp434717@gmail.com>

--- strext/strextensions.c
+++ strext/strextensions.c
@@ -2,22 +2,38 @@
 
 #include <string.h>
 
-#define BUFFER_RESIZE_FACTOR (5)
+typedef struct string_t {
+  char *data;
+  size_t length;
+  size_t allocated_size;
+  double reallocation_multiplier;
+} string_t;
 
-char *findAndReplace(char *const haystack, const size_t HAYSTACK_LEN,
-                     const char *const needle, const size_t NEEDLE_LEN,
-                     const char *const replacer, const size_t REPLACER_LEN) {
-  const ptrdiff_t needleIndex = strstr(haystack, needle) - haystack;
+string_t *findReplace(string_t *const haystack, const string_t *const needle,
+                      const string_t *const replacer) {
+  const ptrdiff_t needleIndex =
+      strstr(haystack->data, needle->data) - haystack->data;
   if (needleIndex >= 0) {
-    char *buffer = malloc(HAYSTACK_LEN + REPLACER_LEN + 1);
+    const size_t BYTES_REQUIRED = haystack->length + replacer->length + 1;
+    char *buffer;
+    if (BYTES_REQUIRED > haystack->allocated_size) {
+      buffer = malloc(BYTES_REQUIRED);
+      haystack->allocated_size =
+          BYTES_REQUIRED * haystack->reallocation_multiplier;
+    } else {
+      buffer = haystack->data;
+    }
     /* Copy chars up to the point of insertion */
-    strncpy(buffer, haystack, needleIndex); 
+    strncpy(buffer, haystack->data, needleIndex);
     /* Insert the replacing string */
-    strcpy(buffer + needleIndex, replacer); 
+    strcpy(buffer + needleIndex, replacer->data);
     /* Copy chars after the point of insertion */
-    strcpy(buffer + needleIndex + REPLACER_LEN, haystack + needleIndex + NEEDLE_LEN);
-    buffer[HAYSTACK_LEN + REPLACER_LEN] = '\0';
-    return buffer;
+    strcpy(buffer + needleIndex + replacer->length,
+           haystack->data + needleIndex + needle->length);
+    buffer[haystack->length + replacer->length] = '\0';
+    haystack->length += replacer->length - needle->length;
+    haystack->data = buffer;
+    return haystack;
   }
   return haystack;
 }
