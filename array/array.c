3389c830623f25e82ca18868e63b43452265e85a
LorenzoPalacios1 <lp434717@gmail.com>
2024-06-17 15:43:31 -0500

    array: rename array type to array_t; rename create_array() to new_array()
    Signed-off-by: LorenzoPalacios1 <lp434717@gmail.com>

--- array/array.c
+++ array/array.c
@@ -1,8 +1,8 @@
 #include "array.h"
 
-array *create_array(const void *const data, const size_t elem_size,
-                    const size_t num_elems) {
-  array *new_arr = malloc(sizeof(array));
+array_t *new_array(const void *const data, const size_t elem_size,
+                   const size_t num_elems) {
+  array_t *new_arr = malloc(sizeof(array_t));
   new_arr->num_elems = num_elems;
   new_arr->allocated_size = (elem_size * num_elems);
   new_arr->elem_size = elem_size;
@@ -12,26 +12,26 @@ array *create_array(const void *const data, const size_t elem_size,
   return new_arr;
 }
 
-void *get_elem(const array *const arr, const size_t index) {
+void *get_elem(const array_t *const arr, const size_t index) {
   if (index >= arr->num_elems) return NULL;
   return (char *)arr->data + (arr->elem_size * index);
 }
 
-void delete_array(array *arr) {
+void delete_array(array_t *arr) {
   free(arr->data);
   free(arr);
   arr = NULL;
 }
 
-void secure_delete_array(array *arr) {
+void secure_delete_array(array_t *arr) {
   memset(arr->data, 0, arr->allocated_size);
   free(arr->data);
-  memset(arr, 0, sizeof(array));
+  memset(arr, 0, sizeof(array_t));
   free(arr);
   arr = NULL;
 }
 
-void clear_array(array *const arr) {
+void clear_array(array_t *const arr) {
   memset(arr->data, 0, arr->allocated_size);
   arr->num_elems = 0;
 }
--- array/array.h
+++ array/array.h
@@ -5,22 +5,22 @@
 
 #include "../MyBasics.h"
 
-typedef struct array {
+typedef struct array_t {
   void *data;
   size_t num_elems;
   size_t allocated_size;
   size_t elem_size;
-} array;
+} array_t;
 
-array *create_array(const void *const data, const size_t elem_size,
-                    const size_t num_elems);
+array_t *new_array(const void *const data, const size_t elem_size,
+                   const size_t num_elems);
 
-void *get_elem(const array *const arr, const size_t index);
+void *get_elem(const array_t *const arr, const size_t index);
 
-void delete_array(array *arr);
+void delete_array(array_t *arr);
 
-void secure_delete_array(array *arr);
+void secure_delete_array(array_t *arr);
 
-void clear_array(array *const arr);
+void clear_array(array_t *const arr);
 
 #endif
