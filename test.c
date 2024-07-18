66a2b05ba495cc5ffb95bd8cd7b390245e636f99
LorenzoPalacios1 <lp434717@gmail.com>
2024-07-18 18:35:58 -0500

    test suite refactoring checkpoint
    Signed-off-by: LorenzoPalacios1 <lp434717@gmail.com>

--- test.c
+++ test.c
@@ -7,7 +7,6 @@
 
 #include "MyBasics.h"
 #include "array/array.h"
-#include "bpio/boilerplateio.h"
 #include "randomgen/randomgen.h"
 #include "strext/strextensions.h"
 
@@ -28,10 +27,15 @@
 
 #define IS_RUN_ALL_TESTS_KEYWORD_CASE_SENSITIVE (false)
 
+#if (!IS_RUN_ALL_TESTS_KEYWORD_CASE_SENSITIVE)
+static char _RUN_ALL_TESTS_KEYWORD[] = RUN_ALL_TESTS_KEYWORD;
+#undef RUN_ALL_TESTS_KEYWORD
+static char *const RUN_ALL_TESTS_KEYWORD = _RUN_ALL_TESTS_KEYWORD;
+#endif
+
 /*
  * - Utility Functions -
  */
-
 // Generates and applies a seed for `rand()`.
 // \returns The applied seed.
 static unsigned random_seeder(void) {
@@ -50,23 +54,27 @@ static unsigned random_seeder(void) {
  */
 
 // MAKE SURE TO UPDATE BOTH ARRAYS
-static clock_t (*const tests[])(void) = {NULL, NULL};
-static const char *test_names[NUM_TESTS] = {"asajsaaj", "asjsajhhd"};
+static clock_t (*const tests[])(void) = {NULL};
+static const char *test_names[NUM_TESTS] = {"string_from_chars"};
 
 static void prompt_user(void) {
   puts("Your test choices are:");
   for (size_t i = 0; i < NUM_TESTS; i++) printf("%zu. %s\n", i, test_names[i]);
-  puts("\nEnter the test number(s) you want to run, or \"" RUN_ALL_TESTS_KEYWORD
-       "\" (case sensitive) to run all of the tests.\n"
-       "Press the enter/return key to start running the selected tests.");
+  printf(
+      "\nEnter the test number(s) you want to run, or \"%s\" to run all "
+      "tests.\n"
+      "Press the enter/return key to complete your selection and begin running "
+      "the selected tests.\n",
+      RUN_ALL_TESTS_KEYWORD);
 }
 
 /*
- * Asks the user to choose from a list of available tests as defined above.
- * \returns An array of `int` where each element corresponds to a given test.
+ * Asks the user to choose from a list of available tests as defined by `tests`
+ * and `test_names`.
+ * \returns An array of `size_t` whose elements each correspond to a test.
  */
 static array_t *get_user_test_selection(void) {
-  int selected_tests[NUM_TESTS];
+  size_t selected_tests[NUM_TESTS];
 
   size_t current_test_num = 0;
   for (; current_test_num < NUM_TESTS; current_test_num++) {
@@ -81,7 +89,7 @@ static array_t *get_user_test_selection(void) {
     }
     string_t *input = string_from_stream(stdin);
 
-#if (IS_RUN_ALL_TESTS_KEYWORD_CASE_SENSITIVE == false)
+#if (!IS_RUN_ALL_TESTS_KEYWORD_CASE_SENSITIVE)
     char *const str = input->data;
     for (size_t i = 0; str[i] != '\0'; i++) str[i] = tolower(str[i]);
 #endif
@@ -97,14 +105,13 @@ static array_t *get_user_test_selection(void) {
   return selected_tests_actual;
 }
 
-static clock_t run_tests(const array_t *const test_selection) {
-  clock_t total_time_taken = 0;
+static size_t run_tests(const array_t *const test_selection) {
+  size_t total_time_taken = 0;
   const int *const test_indexes = test_selection->data;
   for (size_t i = 0; i < test_selection->num_elems; i++) {
-    printf("\nNow running %d - %s\n", test_indexes[i],
-           test_names[test_indexes[i]]);
-    const clock_t test_elapsed_time = tests[test_indexes[i]]();
-    printf("\nFinished running %d - %s\nElapsed time: %ldms\n", test_indexes[i],
+    printf("\nTest #%d (%s)\n", test_indexes[i], test_names[test_indexes[i]]);
+    const size_t test_elapsed_time = tests[test_indexes[i]]();
+    printf("\nTest #%d complete (%s)\nTime taken: %lldms\n", test_indexes[i],
            test_names[test_indexes[i]], test_elapsed_time);
     total_time_taken += test_elapsed_time;
   }
@@ -113,8 +120,11 @@ static clock_t run_tests(const array_t *const test_selection) {
 
 void greet_and_init(void) {
   puts("\n - MyBasics Testing Suite - \n");
-
   printf("rand() Seed: %u\nrand() Sample: %d\n", random_seeder(), rand());
+#if (!RUN_ALL_TESTS_KEYWORD_IS_CASE_SENSITIVE)
+  for (size_t i = 0; RUN_ALL_TESTS_KEYWORD[i] != '\0'; i++)
+    RUN_ALL_TESTS_KEYWORD[i] = tolower(RUN_ALL_TESTS_KEYWORD[i]);
+#endif
 }
 
 int main(void) {
