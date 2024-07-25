0e8386e9047061816e51a4d5b54c884235823750
LorenzoPalacios1 <lp434717@gmail.com>
2024-07-25 17:22:31 -0500

    strext: documentation and minor refactoring
    Signed-off-by: LorenzoPalacios1 <lp434717@gmail.com>

--- strext/strextensions.c
+++ strext/strextensions.c
@@ -4,40 +4,32 @@
 #include <stdio.h>
 #include <string.h>
 
-#define BASE_STR_ALLOC_SIZE (20) /* In bytes. */
-#define BASE_REALLOC_FACTOR (2)
-
-void append_char_to_string(string_t *str_obj, const char appended) {
+string_t *append_char_to_string(string_t *str_obj, const char appended) {
   char *raw_str = str_obj->data;
-  if (str_obj->length == str_obj->str_alloc_size) {
-    string_t *reallocated_mem = resize_string(
-        str_obj, str_obj->realloc_factor * str_obj->str_alloc_size);
-    if (reallocated_mem == NULL) return;
+  if (str_obj->length == str_obj->capacity) {
+    string_t *reallocated_mem = expand_string(str_obj);
+    if (reallocated_mem == NULL) return NULL;
     str_obj = reallocated_mem;
   }
   raw_str[str_obj->length] = appended;
   str_obj->length++;
   raw_str[str_obj->length] = '\0';
+  return str_obj;
 }
 
-/* Expands the passed string's allocated boundaries by `realloc_factor`. */
-string_t *expand_string(string_t *str) {
-  return resize_string(str, str->realloc_factor * str->str_alloc_size);
+string_t *expand_string(string_t *str_obj) {
+  return resize_string(str_obj, str_obj->realloc_factor * str_obj->capacity);
 }
 
-string_t *find_replace(string_t *const haystack, const string_t *const needle,
+string_t *find_replace(string_t *haystack, const string_t *const needle,
                        const string_t *const replacement) {
-  /* These are shorthands for accessing often used variables. */
   const char *const replacer = replacement->data;
   const char *const to_be_replaced = needle->data;
   const size_t REPLACER_LEN = replacement->length;
   const size_t NEEDLE_LEN = needle->length;
   char *hay = haystack->data;
-  /*
-   * The length of the haystack can change during string insertion, hence this
-   * being a pointer instead of a constant.
-   */
-  size_t *const HAY_LEN = &(haystack->length);
+  /* The value of `HAY_LEN` is subject to change during string replacement. */
+  const size_t HAY_LEN = haystack->length;
 
   /*
    * There exists a bug where if `needle` is a zero-length string (that is, a
@@ -46,19 +38,19 @@ string_t *find_replace(string_t *const haystack, const string_t *const needle,
    * Beyond that, this statement only serves to terminate the function if any
    * passed strings are of length 0, since no meaningful operation can be had.
    */
-  if (*HAY_LEN == 0 || NEEDLE_LEN == 0 || REPLACER_LEN == 0) return haystack;
+  if (HAY_LEN == 0 || NEEDLE_LEN == 0 || REPLACER_LEN == 0) return haystack;
 
   const ptrdiff_t needle_index = strstr(hay, to_be_replaced) - hay;
   if (needle_index >= 0) {
     {
-      const size_t BYTES_REQUIRED = *HAY_LEN + REPLACER_LEN - NEEDLE_LEN;
-      if (haystack->str_alloc_size < BYTES_REQUIRED) {
-        haystack->str_alloc_size = BYTES_REQUIRED * haystack->realloc_factor;
-        hay = realloc(hay, haystack->str_alloc_size);
-        if (!hay) return haystack;
+      const size_t BYTES_REQUIRED = REPLACER_LEN - NEEDLE_LEN;
+      if (haystack->capacity < BYTES_REQUIRED) {
+        string_t *reallocated_mem = expand_string(haystack);
+        if (reallocated_mem == NULL) return NULL;
+        haystack = reallocated_mem;
       }
     }
-    char suffixed_chars[*HAY_LEN - needle_index - NEEDLE_LEN];
+    char suffixed_chars[HAY_LEN - needle_index - NEEDLE_LEN];
     /* Copy chars up to the point of insertion. */
     strcpy(suffixed_chars, haystack->data + needle_index + NEEDLE_LEN);
     /* Insert the replacement string. */
@@ -71,33 +63,34 @@ string_t *find_replace(string_t *const haystack, const string_t *const needle,
   return haystack;
 }
 
-/* 
- * This function only modifies the memory allocated for characters, not the
- * rest of the struct.
- * The actual size of any string can be no lower than sizeof(string_t).
- */
-string_t *resize_string(string_t *str, const size_t new_size) {
-  string_t *new_mem = realloc(str, new_size + sizeof(string_t));
+string_t *resize_string(string_t *str_obj, const size_t new_size) {
+  string_t *new_mem = realloc(str_obj, new_size + sizeof(string_t));
   if (new_mem == NULL) return NULL;
-  new_mem->str_alloc_size = new_size;
+  new_mem->capacity = new_size;
   new_mem->data = (char *)new_mem + sizeof(string_t);
   return new_mem;
 }
 
-string_t *shrink_alloc_to_length(string_t *str) {
-  return resize_string(str, str->length);
+string_t *shrink_alloc_to_length(string_t *str_obj) {
+  return resize_string(str_obj, str_obj->length);
 }
 
 string_t *string_from_chars(const char *const raw_text) {
-  string_t *str_obj = malloc(BASE_STR_ALLOC_SIZE + sizeof(string_t));
+  string_t *str_obj = malloc(BASE_STR_CAPACITY + sizeof(string_t));
   if (str_obj == NULL) return NULL;
-  str_obj->str_alloc_size = BASE_STR_ALLOC_SIZE;
+  str_obj->capacity = BASE_STR_CAPACITY;
   str_obj->realloc_factor = BASE_REALLOC_FACTOR;
   str_obj->data = (char *)str_obj + sizeof(string_t);
 
+  /*
+   * `strncpy()` could simplify this loop, but it may introduce overhead as,
+   * after a null terminator is reached, it will "fill in" any unused space
+   * within `str_obj` with null characters. Currently, this loop writes only
+   * what is necessary.
+   */
   size_t i = 0;
   for (; raw_text[i] != '\0'; i++) {
-    if (i == str_obj->str_alloc_size) {
+    if (i == str_obj->capacity) {
       string_t *new_mem = expand_string(str_obj);
       if (new_mem == NULL) return NULL;
       str_obj = new_mem;
@@ -114,14 +107,14 @@ string_t *string_from_line_stdin(void) {
 }
 
 string_t *string_from_stream(FILE *const stream) {
-  string_t *str_obj = new_string(BASE_STR_ALLOC_SIZE);
+  string_t *str_obj = new_string(BASE_STR_CAPACITY);
   char *str_actual = str_obj->data;
 
   char c = getc(stream);
   for (size_t i = 0; c != EOF; i++) {
-    if (i == str_obj->str_alloc_size) {
-      string_t *reallocated_mem = resize_string(
-          str_obj, str_obj->realloc_factor * str_obj->str_alloc_size);
+    if (i == str_obj->capacity) {
+      string_t *reallocated_mem =
+          resize_string(str_obj, str_obj->realloc_factor * str_obj->capacity);
       if (reallocated_mem == NULL) return NULL;
       str_obj = reallocated_mem;
     }
@@ -132,7 +125,7 @@ string_t *string_from_stream(FILE *const stream) {
 }
 
 string_t *string_from_stream_given_delim(const char delim, FILE *const stream) {
-  string_t *const str_obj = new_string(BASE_STR_ALLOC_SIZE);
+  string_t *const str_obj = new_string(BASE_STR_CAPACITY);
   char *const str_actual = str_obj->data;
 
   size_t i = 0;
@@ -148,12 +141,12 @@ string_t *string_from_stream_given_delim(const char delim, FILE *const stream) {
   return str_obj;
 }
 
-string_t *string_preallocated(const size_t size_in_bytes) {
-  string_t *str_obj = malloc(size_in_bytes + sizeof(string_t));
+string_t *string_of_capacity(const size_t capacity) {
+  string_t *str_obj = malloc(capacity + sizeof(string_t));
   if (str_obj == NULL) return NULL;
   str_obj->data = (char *)str_obj + sizeof(string_t);
   str_obj->length = 0;
-  str_obj->str_alloc_size = size_in_bytes;
+  str_obj->capacity = capacity;
   str_obj->realloc_factor = BASE_REALLOC_FACTOR;
   return str_obj;
 }
--- strext/strextensions.h
+++ strext/strextensions.h
@@ -6,70 +6,147 @@
 
 #include "../MyBasics.h"
 
+#define BASE_STR_CAPACITY (8192) /* In bytes. */
+#define BASE_REALLOC_FACTOR (2)
+
 typedef struct string_t {
   char *data;
   size_t length;
-  /*
-   * This is the size of the memory used for character storage, or
-   * alternatively, the maximum number of characters that can be held in this
-   * string without reallocation.
-   * This is NOT the size of the whole struct, which can be found by
-   * `str_alloc_size + sizeof(string_t)`.
-   */
-  size_t str_alloc_size;
+  size_t capacity;
   double realloc_factor;
 } string_t;
 
 /* clang-format off */
 
-#define new_string(arg)                 \
-  (_Generic((arg),                      \
-  char *: string_from_chars,            \
-  FILE *: string_from_stream,           \
-  int: string_preallocated,             \
-  size_t: string_preallocated           \
+#define new_string(arg)                   \
+  (_Generic((arg),                        \
+  char *: string_from_chars,              \
+  FILE *: string_from_stream,             \
+  signed long long: string_of_capacity,   \
+  signed long: string_of_capacity,        \
+  signed int: string_of_capacity,         \
+  signed short: string_of_capacity,       \
+  signed char: string_of_capacity,        \
+  unsigned long long: string_of_capacity, \
+  unsigned long: string_of_capacity,      \
+  unsigned int: string_of_capacity,       \
+  unsigned short: string_of_capacity,     \
+  unsigned char: string_of_capacity       \
   )(arg))
 
 /* clang-format on */
 
-void append_char_to_string(string_t *const str_obj, const char appended);
+/*
+ * Appends `appended` to the end of `str_obj`, expanding if necessary.
+ *
+ * \return A pointer associated with the data of `str_obj`, or `NULL` if
+ * appending failed.
+ */
+string_t *append_char_to_string(string_t *const str_obj, const char appended);
 
-string_t *expand_string(string_t *str);
+/*
+ * Expands the passed string's allocated boundaries by `realloc_factor`.
+ *
+ * \return A (possibly new) pointer associated with the data of `str_obj`, or
+ * `NULL` if reallocation failed.
+ */
+string_t *expand_string(string_t *str_obj);
 
+/*
+ * Finds the first occurrence of `needle` within `haystack` starting from the
+ * beginning of `haystack->data` and replaces it with `replacement`, expanding
+ * `haystack` as necessary.
+ *
+ * \return A (possibly new) pointer associated with the data of `haystack`, or
+ * `NULL` if the operation failed.
+ *
+ * \note A returned `NULL` pointer does not guarantee `haystack` is unmodified
+ * by this function.
+ */
 string_t *find_replace(string_t *const haystack, const string_t *const needle,
                        const string_t *const replacer);
 
-/* To be added. */
+/*
+ * Finds all occurrences of `needle` within `haystack` starting from the
+ * beginning of `haystack->data`, replacing any findings with `replacement` and
+ * expanding `haystack` as necessary.
+ *
+ * \return A (possibly new) pointer associated with the data of `haystack`, or
+ * `NULL` if the operation failed.
+ *
+ * \note A returned `NULL` pointer does not guarantee `haystack` is unmodified
+ * by this function.
+ *
+ * \attention Function not yet complete.
+ */
 string_t *find_replace_all(string_t *const haystack,
                            const string_t *const needle,
                            const string_t *const replacement);
 
-string_t *resize_string(string_t *str, const size_t new_size);
+/*
+ * Reallocates the memory used for `str_obj` to fit `new_size` bytes.
+ *
+ * This function only modifies the memory allocated for characters, meaning
+ * there will always be enough space for the data members of `string_t`
+ * regardless of the value passed as `new_size`.
+ *
+ * \return A (possibly new) pointer associated with the data of `str_obj`, or
+ * `NULL` if reallocation failed.
+ */
+string_t *resize_string(string_t *str_obj, const size_t new_size);
 
-string_t *shrink_alloc_to_length(string_t *str);
+/*
+ * Shrinks the memory used for `str_obj` to fit the number of characters it
+ * contains.
+ *
+ * \return A (possibly new) pointer associated with the data of `str_obj`, or
+ * `NULL` if reallocation failed.
+ */
+string_t *shrink_alloc_to_length(string_t *str_obj);
 
+/*
+ * Generates a `string_t` object whose `data` consists of the passed raw
+ * null-terminated string, `raw_text`.
+ *
+ * \return A `string_t` object containing characters from `raw_text`, or `NULL`
+ * upon failure.
+ */
 string_t *string_from_chars(const char *const raw_text);
 
 /*
- * Returns a string whose content consists of characters within `stdin` until
- * reading a newline character.
+ * Generates a `string_t` object whose `data` consists of a single line of
+ * characters from `stdin`.
+ *
+ * \return A `string_t` object containing characters from `stdin`, or `NULL`
+ * upon failure.
  */
 string_t *string_from_line_stdin(void);
 
+/*
+ * Creates a `string_t` object whose `data` consists of characters within
+ * `stream` until `EOF` is met.
+ *
+ * \return A pointer to a `string_t` object containing characters from `stream`,
+ * or `NULL` upon failure.
+ */
 string_t *string_from_stream(FILE *const stream);
 
 /*
- * \returns A string whose content consists of characters within `stream` until
- * `delim` is read.
+ * Creates a `string_t` object whose `data` consists of characters from `stream`
+ * until either `EOF` or the specified `delim` character is met.
  *
- * \note A null terminator will replace `delim` where it would have occurred
- * within the string.
- *
- * \param delim The delimiter character
- * \param stream The input stream to read from
+ * \return A pointer to a `string_t` object containing characters from `stream`,
+ * or `NULL` upon failure.
  */
 string_t *string_from_stream_given_delim(const char delim, FILE *const stream);
 
-string_t *string_preallocated(const size_t size_in_bytes);
+/*
+ * Creates a `string_t` object capable of storing `capacity` number of
+ * characters.
+ *
+ * \return A pointer to a `string_t` object of size
+ * `capacity + sizeof(string_t)`, or `NULL` upon failure.
+ */
+string_t *string_of_capacity(const size_t capacity);
 
 #endif
