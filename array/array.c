a7d651bc2cded4cc0a0d486daf34bf8a90bb9c58
LorenzoPalacios1 <lp434717@gmail.com>
2024-08-25 01:01:04 -0500

    arrays: minor refactoring
    Signed-off-by: LorenzoPalacios1 <lp434717@gmail.com>

--- array/array.c
+++ array/array.c
@@ -1,37 +1,37 @@
 #include "array.h"
 
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stddef.h>
 
-array_t *new_array(const void *const data, const size_t elem_size,
-                   const size_t num_elems) {
-  array_t *const new_arr = malloc(elem_size * num_elems + sizeof(array_t));
-
-  new_arr->allocated_size = elem_size * num_elems;
+array_t *_new_array(const void *const data, const size_t elem_size,
+                   const size_t length) {
+  array_t *const new_arr = malloc(elem_size * length + sizeof(array_t));
+  new_arr->capacity = elem_size * length;
   new_arr->elem_size = elem_size;
-  new_arr->num_elems = num_elems;
+  new_arr->length = length;
   new_arr->data = (char *)new_arr + sizeof(array_t);
 
-  if (data != NULL) memcpy(new_arr->data, data, elem_size * num_elems);
+  if (data != NULL) memcpy(new_arr->data, data, elem_size * length);
   return new_arr;
 }
 
 void *get_elem(const array_t *const arr, const size_t index) {
-  if (index >= arr->num_elems) return NULL;
+  if (index >= arr->length) return NULL;
   return (char *)arr->data + (arr->elem_size * index);
 }
 
-void delete_array(array_t **const arr) {
+void _delete_array(array_t **const arr) {
   free(*arr);
   *arr = NULL;
 }
 
-void secure_delete_array(array_t **const arr) {
-  memset(*arr, 0, (*arr)->allocated_size + sizeof(**arr));
+void delete_array_s(array_t *arr) {
+  memset(arr, 0, arr->capacity + sizeof(*arr));
   delete_array(arr);
 }
 
 void clear_array_contents(array_t *const arr) {
-  memset(arr->data, 0, arr->allocated_size);
+  memset(arr->data, 0, arr->capacity);
+  arr->length = 0;
 }
--- array/array.h
+++ array/array.h
@@ -3,21 +3,23 @@
 
 #include <stddef.h>
 
+#define new_array(data, length) _new_array(data, sizeof*(data), length)
+#define delete_array(arr) _delete_array(&(arr))
+
 typedef struct array_t {
   void *data;
-  size_t allocated_size;
+  size_t capacity;
+  size_t length;
   size_t elem_size;
-  size_t num_elems;
 } array_t;
 
-array_t *new_array(const void *data, size_t elem_size,
-                   size_t num_elems);
+array_t *_new_array(const void *data, size_t elem_size, size_t length);
 
 void *get_elem(const array_t *arr, size_t index);
 
-void delete_array(array_t **arr);
+void _delete_array(array_t **arr);
 
-void secure_delete_array(array_t **arr);
+void delete_array_s(array_t *arr);
 
 void clear_array_contents(array_t *arr);
 
