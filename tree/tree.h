072d1f1d3f10cb281bec78ef5c91e7142dca11ea
LorenzoPalacios1 <lp434717@gmail.com>
2024-08-25 01:29:30 -0500

    tree: starting rewrite
    Signed-off-by: LorenzoPalacios1 <lp434717@gmail.com>

--- tree/tree.h
+++ tree/tree.h
@@ -1,32 +1,23 @@
-#ifndef _TREE_H
-#define _TREE_H
+#ifndef TREE_H
+#define TREE_H
 
 #include <stdbool.h>
+#include <stddef.h>
 
 typedef struct node_t {
-  void *node_value;
+  void *value;
   struct node_t *parent;
-  struct node_t *children;
-  size_t num_children;
-  size_t node_depth_level;
+  struct node_t **children;
 } node_t;
 
-/*
- * Container structure for a tree data structure.
- * Contains a pointer to the tree's root node as well as some basic
- * statistics about the tree.
- */
+/* Container structure for a generic tree data structure. */
 typedef struct tree_t {
-  node_t *root_node;
-  size_t max_depth;
-  double avg_node_depth;
-
+  node_t *root;
   size_t num_nodes;
-  size_t size_of_node_value_in_bytes;
-
-  node_t *highest_value_node;
-  node_t *lowest_value_node;
+  size_t depth;
 } tree_t;
+
+#define new_tree(data, length) _new_tree(data, sizeof *(data), length)
 
 /*
  * Initializes a tree with the given elements from the passed array.
@@ -42,14 +33,13 @@ typedef struct tree_t {
  *      /     \
  *     D       E
  */
-tree_t *create_tree(const void *arr, size_t arr_length,
-                    size_t elem_size_in_bytes);
+tree_t *_new_tree(const void *data, size_t elem_size, size_t length);
 
 /*
- * Frees the entire passed tree's consumed memory and assigns a NULL
+ * Frees the entire passed tree's consumed memory and assigns a `NULL`
  * pointer value to the root.
  */
-void delete_tree(tree_t *tree_to_delete);
+void delete_tree(tree_t **tree_to_delete);
 
 /*
  * Same as `delete_tree()`, except this function will set all allocated memory
@@ -65,22 +55,18 @@ bool secure_delete_tree(tree_t *tree_to_delete);
 bool add_node(tree_t *parent_tree, const void *elem);
 
 /*
- * Removes the specified element from the passed tree.
- * This function will update the tree's statistics if the element was
- * successfully added to the tree.
+ * Removes `target` node from `src`, thereby causing `target` to have no parent
+ * (assumed `NULL`).
  *
- * Returns `true` if the element was successfully removed, and false
- * otherwise.
+ * \return The removed node.
  */
-node_t remove_node(tree_t *parent_tree, const void *elem);
+node_t *remove_node(tree_t *src, node_t *target);
 
 /*
  * This function takes the first argument and re-parents it to the
  * second argument, including any child nodes with it.
- *
- * Returns `true` if the re-parenting was successful, and false otherwise.
  */
-bool make_node_child_of(const node_t *node_to_be_moved,
+void make_node_child_of(const node_t *node_to_be_moved,
                         const node_t *new_parent);
 
 /*
