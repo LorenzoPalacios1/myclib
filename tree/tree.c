e549d64e56c627888f312c54eb8a4dad38c893f5
LorenzoPalacios1 <lp434717@gmail.com>
2024-08-31 19:14:57 -0500

    tree.c: _new_tree() prototype
    Signed-off-by: LorenzoPalacios1 <lp434717@gmail.com>

--- tree/tree.c
+++ tree/tree.c
@@ -8,7 +8,20 @@
 tree_t *_new_tree(const void *const data, const size_t elem_size,
                   const size_t length) {
   const size_t NODE_SIZE = sizeof(node_t) + elem_size;
-  const size_t REQUIRED_MEM = length * NODE_SIZE + sizeof(tree_t);
+  /*
+   * The first portion of the below expression determines the minimum amount of
+   * memory required for the tree and its nodes. The second portion determines
+   * the minimum amount of memory required to contain a list of each node's
+   * children.
+   *
+   * `length - 1` exists in the second expression since the root node will have
+   * two children, while every other node will have only one. Meanwhile, the
+   * last two will have no children. This gives the unmodified expression
+   *
+   * (n + 1 - 2) * ptr_size
+   */
+  const size_t REQUIRED_MEM =
+      (length * NODE_SIZE + sizeof(tree_t)) + ((length - 1) * sizeof(node_t *));
   tree_t *const tree_obj = malloc(REQUIRED_MEM);
   if (tree_obj == NULL) return NULL;
 
@@ -24,15 +37,17 @@ tree_t *_new_tree(const void *const data, const size_t elem_size,
   memcpy(nodes_heap->value, data, elem_size);
   tree_obj->root = nodes_heap;
   for (size_t i = 1; i < length; i++) {
-    nodes_heap += NODE_SIZE;
-    /* This expression exists to give the tree the structure shown in the
-     * documentation's example. */
+    nodes_heap = (node_t *)((char *)nodes_heap + NODE_SIZE);
+    /*
+     * This expression exists to give the nodes the structure shown in the
+     * documentation's example. The ensures the root node is the parent to two
+     * nodes, after which all other nodes are the parent to at most one node.
+     */
     node_t *const parent_node =
-        (node_t *)((char *)nodes_heap - (i & 1 ? NODE_SIZE : 2 * NODE_SIZE));
+        (node_t *)((char *)nodes_heap - (i == 2 ? 2 * NODE_SIZE : NODE_SIZE));
     nodes_heap->parent = parent_node;
     nodes_heap->value = (char *)nodes_heap + sizeof(node_t);
     memcpy(nodes_heap->value, (char *)data + i * elem_size, elem_size);
-    printf("%d\n", (int)(*(int *)parent_node->value));
   }
   return tree_obj;
 }
@@ -41,6 +56,5 @@ int main(void) {
   int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   tree_t *a = new_tree(data, sizeof(data) / sizeof(*data));
   printf("%d\n", (int)(*(int *)a->root->value));
-  putchar('a');
   return 0;
 }
