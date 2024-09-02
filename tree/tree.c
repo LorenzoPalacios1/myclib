c4dfef32f56926c38a620f7104451d8c2347d4b4
LorenzoPalacios1 <lp434717@gmail.com>
2024-09-01 20:48:00 -0500

    tree.c: fix bug causing the third node to be at a negative index of the root node's children
    Signed-off-by: LorenzoPalacios1 <lp434717@gmail.com>

--- tree/tree.c
+++ tree/tree.c
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
 tree_t *_new_tree(const void *const data, const size_t elem_size,
                   const size_t length) {
@@ -16,10 +15,10 @@ tree_t *_new_tree(const void *const data, const size_t elem_size,
    * node's children.
    *
    * `length - 1` exists in the second expression since the root node will have
-   * two children, while every other node will have only one, except for the
-   * last two, which will have no children. This gives the expression:
+   * at most two children, while every other node will have at most one, except
+   * for the last two, which will have no children. This gives the expression:
    *
-   * (n + 1 - 2) * ptr_size
+   * `(n + 1 - 2) * ptr_size`
    *
    * where `n` is the number of nodes.
    */
@@ -37,13 +36,13 @@ tree_t *_new_tree(const void *const data, const size_t elem_size,
                                       ((length - 1) * sizeof(node_t *)));
   nodes_mem[0].parent = NULL;
   nodes_mem[0].num_children = 0;
-  nodes_mem[0].value = (char *)nodes_mem + sizeof(node_t);
+  nodes_mem[0].value = nodes_mem + 1;
   memcpy(nodes_mem[0].value, data, elem_size);
   tree_obj->root = nodes_mem;
   nodes_mem = (node_t *)((char *)nodes_mem + NODE_SIZE);
   for (size_t i = 1; i < length; i++) {
     nodes_mem->num_children = 0;
-    nodes_mem->value = (char *)nodes_mem + sizeof(node_t);
+    nodes_mem->value = nodes_mem + 1;
     memcpy(nodes_mem->value, (char *)data + i * elem_size, elem_size);
     /*
      * This ensures the root node is the parent to two nodes and that all other
@@ -51,15 +50,20 @@ tree_t *_new_tree(const void *const data, const size_t elem_size,
      */
     node_t *parent_node;
     if (i <= 2)
-      parent_node = (node_t *)((char *)nodes_mem - NODE_SIZE);
+      parent_node = tree_obj->root;
     else
       parent_node = (node_t *)((char *)nodes_mem - 2 * NODE_SIZE);
     nodes_mem->parent = parent_node;
     children_mem[0] = nodes_mem;
-    parent_node->children = children_mem;
+    /* 
+     * This statement ensures the third node becomes a child of the tree's root
+     * node. Until I better revise my design, this ugly hack will have to stay.
+     */
+    if (i != 2) parent_node->children = children_mem;
     parent_node->num_children++;
-    children_mem = (node_t **)((char *)children_mem + sizeof(node_t *));
+
     nodes_mem = (node_t *)((char *)nodes_mem + NODE_SIZE);
+    children_mem++;
   }
   return tree_obj;
 }
@@ -69,7 +73,29 @@ void _delete_tree(tree_t **const tree) {
   *tree = NULL;
 }
 
-void _secure_delete_tree(tree_t **tree) {
+void _secure_delete_tree(tree_t **const tree) {
   memset(*tree, 0, (*tree)->bytes_allocated);
   _delete_tree(tree);
+}
+
+node_t *remove_node(tree_t *const src, node_t *const target) {
+  node_t *const target_parent = target->parent;
+  for (size_t i = 0; i < target_parent->num_children; i++) {
+    if (target_parent->children[i] == target) {
+      memcpy(target_parent->children + i, target_parent->children + i + 1,
+             target_parent->num_children - i);
+      break;
+    }
+  }
+  src->num_nodes -= target->num_children + 1;
+  target_parent->num_children--;
+  target->parent = NULL;
+  return target;
+}
+
+int main(void) {
+  static const int data[] = {1, 2, 3, 4, 5, 6};
+  tree_t *a = new_tree(data, sizeof data / sizeof *data);
+  printf("%d\n", *(int *)a->root->children[0]->value);
+  return 0;
 }
