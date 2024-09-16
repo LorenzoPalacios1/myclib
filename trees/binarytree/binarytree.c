8ef387be51b517366d51b70648c72e2885b674ab
LorenzoPalacios1 <lp434717@gmail.com>
2024-09-15 21:19:52 -0500

    _new_binary_tree(): testing new children algorithm
    Signed-off-by: LorenzoPalacios1 <lp434717@gmail.com>

--- trees/binarytree/binarytree.c
+++ trees/binarytree/binarytree.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "../trees.h"
+
 binary_tree_t *_new_binary_tree(const void *const data, const size_t elem_size,
                                 const size_t length) {
   const size_t NODE_SIZE = sizeof(binary_node_t) + elem_size;
@@ -16,22 +18,24 @@ binary_tree_t *_new_binary_tree(const void *const data, const size_t elem_size,
   tree_obj->bytes_allocated = REQUIRED_MEM;
   tree_obj->bytes_used = REQUIRED_MEM;
 
-  binary_node_t *const nodes_mem =
-      (binary_node_t *)((char *)tree_obj + sizeof(binary_tree_t));
+  binary_node_t *const nodes_mem = (void *)(tree_obj + 1);
   for (size_t i = 0; i < length; i++) {
-    binary_node_t *const cur_node = nodes_mem + i * NODE_SIZE;
+    binary_node_t *const cur_node = (void *)((char *)nodes_mem + i * NODE_SIZE);
     binary_node_t *parent_node;
     if (i == 0)
       parent_node = NULL;
-    else if (i == 1)
-      parent_node = cur_node - NODE_SIZE;
-    else
-      parent_node = cur_node - NODE_SIZE * (i & 1);
-    3 & 1;
+    else {
+      /* Ensures all odd nodes become the left child of `parent_node`. */
+      parent_node = (void*)((char*)cur_node - NODE_SIZE * ((~i & 1) + 1));
+      parent_node->children[~i & 1] = cur_node;
+      printf("parent node value: %d\n", (int)*(int *)parent_node->value);
+    }
+    cur_node->parent = parent_node;
     char *const cur_node_data = (char *)nodes_mem + sizeof(binary_node_t);
     memcpy(cur_node_data, (char *)data + i * elem_size, elem_size);
     nodes_mem->value = cur_node_data;
   }
+  exit(0);
   tree_obj->root = nodes_mem; /* The first node is always the root node. */
   return tree_obj;
 }
@@ -43,15 +47,15 @@ void delete_binary_tree(binary_tree_t **const tree) {
 
 void delete_binary_tree_s(binary_tree_t **const tree) {
   memset(*tree, 0, (*tree)->bytes_allocated);
-  _delete_tree(tree);
+  delete_binary_tree(tree);
 }
 
 binary_node_t *unparent_node(binary_tree_t *const src,
-                             binary_node_t *const target) {}
+                             binary_node_t *const target);
 
 int main(void) {
   static const int data[] = {1, 2, 3, 4, 5, 6};
-  binary_tree_t *a = new_tree(data, sizeof data / sizeof *data);
+  binary_tree_t *a = new_binary_tree(data, sizeof data / sizeof *data);
   printf("%d\n", *(int *)a->root->children[0]->value);
   remove_node(a, a->root->children[0]);
   printf("%d\n", *(int *)a->root->children[0]->value);
