77fbc09670aa3b601ec11d5a3966b146a52620a8
LorenzoPalacios1 <lp434717@gmail.com>
2024-09-27 23:13:57 -0500

    prototype functions added to binarytree; untested
    Signed-off-by: LorenzoPalacios1 <lp434717@gmail.com>

--- trees/binarytree/binarytree.c
+++ trees/binarytree/binarytree.c
@@ -63,14 +63,136 @@ bt_node *remove_node_from_tree(binary_tree *const tree, bt_node *target) {
   return removed_copy;
 }
 
-void delete_node_and_ancestry(bt_node *target) {
+/*
+ * Finds the longest ancestral path of `origin`. This function searches both the
+ * `left` and `right` branches of `origin`.
+ *
+ * \return The maximum ancestral depth of `origin`.
+ */
+size_t get_depth(bt_node *const origin) {
+  size_t left_branch_depth = 0;
+  for (bt_node *cur_node = origin->left; cur_node != NULL;
+       left_branch_depth++) {
+    if (cur_node->left == NULL)
+      cur_node = cur_node->right;
+    else
+      cur_node = cur_node->left;
+  }
 
+  size_t right_branch_depth = 0;
+  for (bt_node *cur_node = origin->right; cur_node != NULL;
+       right_branch_depth++) {
+    if (cur_node->right == NULL)
+      cur_node = cur_node->left;
+    else
+      cur_node = cur_node->right;
+  }
+  return left_branch_depth > right_branch_depth ? left_branch_depth
+                                                : right_branch_depth;
 }
 
-void delete_node(bt_node *target) {
-  bt_node *parent = target->parent;
-  bt_node *new_slot = parent->left;
-  while (new_slot == NULL) {
+/* Calculates the number of ancestor nodes linked to `origin`.
 
+ * \return The total amount of ancestor nodes connected to `origin`.
+*/
+size_t get_num_ancestral_nodes(bt_node *const origin) {
+  size_t count = 0;
+  for (bt_node *cur_node = origin->left; cur_node != NULL; count++) {
+    if (cur_node->left == NULL)
+      cur_node = cur_node->right;
+    else
+      cur_node = cur_node->left;
+  }
+
+  size_t right_branch_depth = 0;
+  for (bt_node *cur_node = origin->right; cur_node != NULL; count++) {
+    if (cur_node->right == NULL)
+      cur_node = cur_node->left;
+    else
+      cur_node = cur_node->right;
+  }
+  return count;
+}
+
+void delete_node_and_ancestry(binary_tree *const tree, bt_node *target) {
+  {
+    bt_node *parent = target->parent;
+    if (parent->left == target)
+      parent->left = NULL;
+    else
+      parent->right = NULL;
+  }
+  const size_t REMOVED_NODES = get_depth(target) + 1;
+  tree->num_nodes -= REMOVED_NODES;
+  tree->used_allocation -= REMOVED_NODES * tree->node_size;
+}
+
+void delete_node_and_ancestry_s(binary_tree *const tree, bt_node *target) {
+  {
+    bt_node *parent = target->parent;
+    if (parent->left == target)
+      parent->left = NULL;
+    else if (parent->right == target)
+      parent->right = NULL;
+  }
+
+  bt_node *left_node = target->left;
+  bt_node *right_node = target->right;
+}
+
+/*
+ * Searches along the `left` ancestors of `origin` until `target` pointer is
+ * found.
+ *
+ * \return A pointer to `target` or `NULL` if `target` was not found.
+ */
+static bt_node **node_search_left(bt_node *origin, bt_node *const target) {
+  while (origin != NULL) {
+    if (origin->left == target) return &origin->left;
+    if (origin->right == target) return &origin->right;
+    origin = origin->left;
+  }
+  return NULL;
+}
+
+/*
+ * Searches along the `right` ancestors of `origin` until `target` pointer is
+ * found.
+ *
+ * \return A pointer to `target` or `NULL` if `target` was not found.
+ */
+static bt_node **node_search_right(bt_node *origin, bt_node *const target) {
+  while (origin != NULL) {
+    if (origin->left == target) return &origin->left;
+    if (origin->right == target) return &origin->right;
+    origin = origin->right;
+  }
+  return &target->right;
+}
+
+/*
+ * Finds the first open slot (that is, a `left` or `right` pointer whose value
+ * is `NULL`) in a branch of nodes.
+ *
+ * \return A pointer to an open slot.
+ * \note If this function encounters a node whose `left` and `right` pointers
+ * are both `NULL`, this function will return a pointer to the `left` pointer.
+ */
+static bt_node **find_open_ancestor(bt_node *const origin) {
+  bt_node **open_slot = node_search_left(origin, NULL);
+  if (open_slot == NULL) node_search_right(origin, NULL);
+  return open_slot;
+}
+
+void make_node_child_of(bt_node *const dst, bt_node *const src) {
+  if (dst->left == NULL) {
+    dst->left = src;
+    return;
+  }
+  if (dst->right == NULL) {
+    dst->right = src;
+    return;
   }
 }
+
+void delete_node(bt_node *target) { bt_node *parent = target->parent; }
