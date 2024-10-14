64b048ef0e240ec2142ccdd4f300beb9a61600d7
LorenzoPalacios1 <lp434717@gmail.com>
2024-10-14 15:12:51 -0500

    example usage of clear_stack(), resize_stack()

--- stack/example.c
+++ stack/example.c
@@ -20,6 +20,7 @@ int main(void) {
     const size_t DATA_LEN = sizeof(data) / sizeof(*data);
     const size_t DATA_ELEM_SIZE = sizeof(*data);
     stack *stk_from_arr = _stack_from_arr(data, DATA_LEN, DATA_ELEM_SIZE);
+    if (stk_from_arr == NULL) return 1;
 
     /* Printing and popping all of the values in `stk_from_arr`. */
 
@@ -44,6 +45,7 @@ int main(void) {
   {
     const int data[] = {1, 2, 3, 4, 5};
     stack *stk_from_arr_macro = stack_from_arr(data);
+    if (stk_from_arr_macro == NULL) return 1;
 
     /* Printing and popping all of the values in `stk_from_arr_macro`. */
 
@@ -71,6 +73,7 @@ int main(void) {
 
     const size_t INITIAL_NUM_ELEMS = 5;
     stack *stk_no_init_data = new_stack(INITIAL_NUM_ELEMS, DATA_ELEM_SIZE);
+    if (stk_no_init_data == NULL) return 1;
 
     /* Pushing the elements of `data` to `stk_no_init_data`. */
     for (size_t i = 0; i < DATA_LEN; i++) {
@@ -106,6 +109,70 @@ int main(void) {
 
     delete_stack(&stk_no_init_data);
     putchar('\n');
+  }
+
+  /*
+   * Example usage of `clear_stack()`.
+   *
+   * This snippet should output:
+   *
+   * `1 (N/A: NULL dereference)`
+   */
+  {
+    const int data[] = {1, 2, 3, 4, 5};
+    stack *example_stack = stack_from_arr(data);
+
+    /* Print the top value of `example_stack`. */
+    int *val = stack_peek(example_stack);
+    if (val == NULL) return 1;
+    printf("%d ", *val);
+
+    clear_stack(example_stack);
+
+    /* Attempt to print the top value of `example_stack` again. */
+    val = stack_peek(example_stack);
+    if (val == NULL) printf("(N/A: val is NULL)");
+
+    delete_stack(&example_stack);
+    putchar('\n');
+  }
+
+  /*
+   * Example usage of `resize_stack()`.
+   *
+   * This snippet could output:
+   *
+   * 4 1600 36 1300
+   */
+  {
+    const size_t elem_size = sizeof(int);
+    size_t num_elems = 1;
+    stack *example_stack = new_stack(num_elems, elem_size);
+    if (example_stack == NULL) return 1;
+
+    /* Initial capacity. */
+    printf("%zu ", example_stack->capacity);
+
+    /* Upscaling by a factor of `example_stack->elem_size`. */
+    num_elems = 400;
+    example_stack = resize_stack(example_stack, elem_size * num_elems);
+    if (example_stack == NULL) return 1;
+    /* New, greater capacity*/
+    printf("%zu ", example_stack->capacity);
+
+    /* Downscaling by a factor of `example_stack->elem_size`. */
+    num_elems = 5;
+    example_stack = resize_stack(example_stack, 7 * num_elems);
+    if (example_stack == NULL) return 1;
+    printf("%zu ", example_stack->capacity);
+
+    /* Upscaling by a non-factor of `example_stack->elem_size`. */
+    num_elems = 100;
+    example_stack = resize_stack(example_stack, 13 * num_elems);
+    if (example_stack == NULL) return 1;
+    printf("%zu ", example_stack->capacity);
+
+    /* Downscaling by a non-factor of `example_stack->elem_size`. */
   }
 
   return 0;
