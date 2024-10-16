e9b7284942895da6e8f6a2cc744c4564f775869a
LorenzoPalacios1 <lp434717@gmail.com>
2024-10-15 22:14:58 -0500

    creating stack as an interface

--- stack/stack.c
+++ stack/stack.c
@@ -126,11 +126,11 @@ stack *stack_push(stack *stk, const void *const elem) {
   return stk;
 }
 
-void *no_heap_stack_peek(stack *stk) { return stack_peek(stk); }
+void *no_heap_stack_peek(stack *const stk) { return stack_peek(stk); }
 
-void *no_heap_stack_pop(stack *stk) { return stack_pop(stk); }
+void *no_heap_stack_pop(stack *const stk) { return stack_pop(stk); }
 
-stack *no_heap_stack_push(stack *stk, const void *const elem) {
+stack *no_heap_stack_push(stack *const stk, const void *const elem) {
   if (stk->used_capacity + stk->elem_size > stk->capacity) return NULL;
   const size_t LENGTH = stk->length;
   const size_t ELEM_SIZE = stk->elem_size;
--- stack/stack.h
+++ stack/stack.h
@@ -58,6 +58,42 @@ void delete_stack_s(stack **stk);
 stack *expand_stack(stack *stk);
 
 /*
+ * Creates a new stack whose `stack->data` is `data`; this function does not
+ * copy the elements of `data` to an allocated block of memory. This function
+ * allocates memory solely for a stack header (that is, the `stack` struct
+ * type).
+ *
+ * \return A pointer to a stack header whose contents are associated with the
+ * values at `data` or `NULL` upon failure.
+ * \note Any changes made to the stack (particularly `stack_interface_push()`)
+ * are reflected upon the contents of `data`.
+ */
+stack *interface_stack(const void *data, size_t len, size_t elem_size);
+
+/*
+ * Returns and removes the top element from `stk`.
+ *
+ * \return A pointer to the top element in `stk` or `NULL` if the end of the
+ * stack was reached.
+ */
+void *interface_stack_pop(stack *stk);
+
+/*
+ * Returns, but does not remove, the top element of `stk`.
+ *
+ * \return A pointer to the top element in `stk` or `NULL` if the end of the
+ * stack was reached.
+ */
+void *interface_stack_peek(stack *stk);
+
+/*
+ * Adds `elem` to `stk` if space permits (this can only be true if
+ * `interface_stack_pop(stk)` was called). `elem` will then be the new top
+ * element and will be returned by functions such as `interface_stack_peek()`.
+ */
+void interface_stack_push(stack *stk, const void *elem);
+
+/*
  * Creates a new `stack` with enough capacity for `num_elems` elements
  * where each element is of size `elem_size`. The stack will be empty.
  *
@@ -84,7 +120,7 @@ stack *resize_stack(stack *stk, size_t new_size);
 stack *shrink_stack_to_fit(stack *stk);
 
 /*
- * Returns the top element of `stk` without removing it.
+ * Returns, but does not remove, the top element of `stk`.
  *
  * \return A pointer to the top element in `stk` or `NULL` if the end of the
  * stack was reached.
@@ -128,7 +164,7 @@ stack *stack_push(stack *stk, const void *const elem);
    .elem_size = _elem_size,                                                   \
    .length = 0};                                                              \
   byte_t GET_STACK_NAME(stk_id)[GET_STACK_ALLOC_SIZE(num_elems, _elem_size)]; \
-  (stk_id).data = GET_STACK_NAME(stk_id) + sizeof(stk_id);
+  (stk_id).data = GET_STACK_NAME(stk_id) + sizeof(stk_id)
 
 /*
  * Creates a stack of automatic storage duration which allocates memory solely
@@ -138,8 +174,6 @@ stack *stack_push(stack *stk, const void *const elem);
  * \note This stack will not modify the allocation of memory at `data`,
  * however it can modify the contents of `data` through `no_heap_stack_pop()`
  * and `no_heap_stack_push()`.
- * \note Unlike `new_stack_no_heap()`, this macro can be directly assigned to
- * a stack definition.
  */
 #define stack_interface(_data, num_elems, _elem_size) \
   {.data = _data,                                     \
@@ -147,8 +181,9 @@ stack *stack_push(stack *stk, const void *const elem);
    .used_capacity = (num_elems) * (_elem_size),       \
    .elem_size = _elem_size,                           \
    .length = num_elems}
+
 /*
- * Returns the top element of `stk` without removing it.
+ * Returns, but does not remove, the top element of `stk`.
  *
  * \return A pointer to the top element in `stk` or `NULL` if the end of the
  * stack was reached.
